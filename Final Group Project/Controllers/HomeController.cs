b2c2b0827885227a4cb1f721e7898ccc7abaa8c5
CJ Marshall <cmarshall2@my.spscc.edu>
2018-11-28 16:24:24 -0800

    Completed DB functionality.

--- Final Group Project/Controllers/HomeController.cs
+++ Final Group Project/Controllers/HomeController.cs
@@ -12,10 +12,10 @@ namespace Final_Group_Project.Controllers
 {
     public class HomeController : Controller
     {
-        ScoresDBEntities1 db;
+        ScoresDBEntities2 db;
         public HomeController()
         {
-            db = new ScoresDBEntities1();
+            db = new ScoresDBEntities2();
         }
 
         public ActionResult Index()
@@ -24,20 +24,38 @@ namespace Final_Group_Project.Controllers
             return View();
         }
 
-        public ActionResult Quiz_Complete()
+        public ActionResult Quiz_Complete(string quiz)
         {
+            ViewBag.Quiz = quiz;
             return View();
+        }
+
+        public ActionResult Save(string quiz, string playerName, int quizScore)
+        {
+            Scores score = new Scores();
+            score.PlayerName = playerName;
+            score.QuizScore = quizScore;
+            score.QuizType = quiz;
+            score.QuizDate = DateTime.Now;
+
+            db.Scores.Add(score);
+            var errors = db.GetValidationErrors();
+            db.SaveChanges();
+            return RedirectToAction("index");
+
         }
         //found info on how to load xml into mvc here http://www.aspmantra.com/2017/03/read-xml-file-and-display-data-using-asp.net-mvc-aspmantra.html
 
-        public ActionResult Quiz(int id)
+        public ActionResult Quiz(int id, string quiz)
         {
             XmlDocument doc = new XmlDocument();
             Question question = new Question();
-            string path = Server.MapPath("~/Content/Questions.xml");
+            //load xml document based on which button the user selected
+            string path = Server.MapPath("~/Content/" + quiz + ".xml");
             doc.Load(path);
+            //find the correct question based on id passed in url
             var node = doc.SelectNodes("/questionList/question")[id - 1];
-
+            //create a c# object out of the xml node
             question.ID = id;
             question.Title = node["title"].InnerText;
             List<string> list = new List<string>();
@@ -56,6 +74,9 @@ namespace Final_Group_Project.Controllers
                 }
             }
             question.answers = list;
+            //preserve the quiz that the user has selected for future questions
+            ViewBag.Quiz = quiz;
+            
             return View(question);
             
         }
